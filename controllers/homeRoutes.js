3cb978d31cffdb8d5482b930a983607b933f1fda
Matthew Tobin <tobin.matthew94@gmail.com>
2023-02-12 12:49:30 -0800

    added view files for the homepage and populated page with the header, navbar, and post data

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,26 +6,58 @@ router.get('/', async (req, res) => {
     try {
         //get all the posts
         const postData = await Post.findAll({
-            // include: [
-            //     {
-            //         model: User,
-            //         attributes: ['username'],
-            //     },
-            // ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
         });
 
         // Serialize the data for the posts to be used in the handlebar template
         const posts = postData.map((post) => post.get({ plain: true }));
+        console.log(posts);
 
-        // Render the homepage and set the logged_in variable
+        // Render the homepage and set the loggedIn variable
         res.render('homepage', {
             posts,
-            logged_in: req.session.logged_in
+            loggedIn: req.session.loggedIn
         });
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+router.get('./post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            post,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+})
 
 module.exports = router;
 
--- models/index.js
+++ models/index.js
@@ -11,13 +11,21 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-  onDelete: 'SET NULL'
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+  onDelete: 'SET NULL'
 });
 
 module.exports = { User, Post, Comment };
